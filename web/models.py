a545548e3098c8d3cc8db29f8df949c3bbcb445e
Erfan <erfan@Erfans-MacBook-Air.local>
2023-07-24 10:01:06 +0330

    initail products model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    Name = models.CharField(max_length=255)
+    Price = models.BigIntegerField(max_length=255)
+    photo = models.ImageField(upload_to=None, max_length=100, height_field=None, width_field=None)
+    def __str__(self) -> str:
+        return self.Name
