5773b16bee77df8346fdb5c77f6ff5761add3d31
Erfan <erfan@Erfans-MacBook-Air.local>
2023-07-24 20:18:09 +0330

    initial products template

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name="HomePage")
+#    path('', views.HomePage.as_view(), name="HomePage"),
+    path('', views.Products_View.as_view(), name="Products")
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+from .models import Product
 # Create your views here.
 
 class HomePage(TemplateView):
     template_name = "HomePage.html"
+
+class Products_View(ListView):
+    model = Product
+    template_name = "Products.html"
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     print("CONTEXT:", context)
+    #     return context
+    
