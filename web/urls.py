f3c32b909ad8d06eb1439761c92e8356b09697ea
Erfan <erfan@Erfans-MacBook-Air.local>
2023-07-30 17:59:15 +0330

    initial login

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
 #    path('', views.HomePage.as_view(), name="HomePage"),
     path('', views.Type_View.as_view(), name="Products"),
-    path('submit/user/', views.NewUser, name="new_user")
+    path('submit/user/', views.NewUser, name="new_user"),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
